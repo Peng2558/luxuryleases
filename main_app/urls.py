4d28b57f8b5b6f001ccbd579d8ed55f961afa55c
mikega1354 <tabrizimike@gmail.com>
2024-01-03 15:55:27 -0800

    Adds rental page

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('about/', views.about, name='about'),
     path('cars/',views.cars_index, name='cars_index'),
+    path('rentals/create', views.RentalCreate.as_view(),name='rentals_create')
 
 
 ]
