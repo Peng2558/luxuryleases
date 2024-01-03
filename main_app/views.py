d8b63cf2f857f048cea6dedd96baab6b8222f2f5
mikega1354 <tabrizimike@gmail.com>
2024-01-03 15:02:17 -0800

    Adds urls and views for about car_index page

--- main_app/views.py
+++ main_app/views.py
@@ -18,6 +18,17 @@ class SignUpForm(UserCreationForm):
 def home(request):
     return render(request, 'home.html')
 
+def about(request):
+  return render(request, 'about.html')
+
+def cars_index(request):
+  cars= Car.objects.all()
+  return render(request,'cars/index.html',{
+      'cars':cars
+  })
+
+
+
 def signup(request):
     error_message = ''
     if request.method == 'POST':
