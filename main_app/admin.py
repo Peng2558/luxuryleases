6211ef42f0a6d2610c3557398ebec3bc089ba2a7
jfoster <jfoster191@hotmail.com>
2024-01-03 13:16:35 -0500

    creates admin user

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Car, Rental, CreditCard, Store
 # Register your models here.
+
+admin.site.register(Car)
+admin.site.register(Rental)
+admin.site.register(CreditCard)
+admin.site.register(Store)
