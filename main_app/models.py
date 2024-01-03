6d67e8891fe57f93c71bf402c47dcaaad54e974e
jfoster <jfoster191@hotmail.com>
2024-01-03 13:02:12 -0500

    creates models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Car(models.Model):
+  make = models.CharField(max_length=50)
+  model = models.CharField(max_length=50)
+  year = models.IntegerField()
+  license_plate = models.CharField(max_length=10)
+  mileage = models.IntegerField()
+
+  def __str__(self):
+    return f'{self.make} {self.model}, Plate: {self.license_plate}'
+  
+class Store(models.Model):
+  name = models.CharField(max_length=100)
+  address = models.CharField(max_length=200, unique=True)
+
+  def __str__(self):
+    return f'{self.name}'
+
+class CreditCard(models.Model):
+  card_number = models.BigIntegerField()
+  card_type = models.CharField(max_length=20)
+  card_holder = models.ForeignKey(User, on_delete=models.CASCADE)
+  expiration_date = models.DateField()
+  cvv = models.IntegerField()
+
+  def __str__(self):
+    return f'{self.card_holder.last_name} {self.card_type}'
+
+class Rental(models.Model):
+  pickup_date = models.DateField()
+  dropoff_date = models.DateField()
+  store = models.ForeignKey(Store, on_delete=models.CASCADE)
+  car = models.ForeignKey(Car, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  rental_fee = models.FloatField()
+  card_on_file = models.ForeignKey(CreditCard, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.user.last_name} - {self.car} - Start: {self.pickup_date}'
