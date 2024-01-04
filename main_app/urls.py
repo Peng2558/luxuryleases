a37cfb321e6fef285fe2f20fa66477ef8d3cc70c
jfoster <jfoster191@hotmail.com>
2024-01-04 12:50:05 -0500

    adds changes to rental form

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('about/', views.about, name='about'),
     path('cars/',views.cars_index, name='cars_index'),
-    path('rentals/create', views.RentalCreate.as_view(),name='rentals_create')
+    path('rentals/create', views.rentals_create,name='rentals_create')
 
 
 ]
